6a1094532a832e003895d78199c47a5772325b6b
AndreiB6 <112687808+AndreiB6@users.noreply.github.com>
2022-12-08 21:32:02 +0200

    add r/p/s ui

--- script.js
+++ script.js
@@ -1,68 +1,71 @@
-function getComputerChoice() {
-    let computerChoice = Math.floor(Math.random() * 3);
-    if (computerChoice === 0) {
-        return 'rock';
-    } else if (computerChoice === 1) {
-        return 'paper';
-    } else if (computerChoice === 2) {
-        return 'scissors';
-    }
-}
-
-let playerSelection;
-let computerSelection;
 let playerScore = 0;
 let computerScore = 0;
+let playerSelection;
+let computerSelection;
 
-function playRound(playerSelection, computerSelection) {
-    if (playerSelection === 'rock' && computerSelection === 'paper') {
-        playerScore ++;
-        return 'winner';
-    } else if (playerSelection === 'rock' && computerSelection === 'scissors') {
-        computerScore ++;
-        return 'loser';
-    } else if (playerSelection === 'paper' && computerSelection === 'rock') {
-        playerScore ++;
-        return 'winner';
-    } else if (playerSelection === 'paper' && computerSelection === 'scissors') {
-        computerScore ++;
-        return 'loser';
-    } else if (playerSelection === 'scissors' && computerSelection === 'paper') {
-        playerScore ++;
-        return 'winner';
-    } else if (playerSelection === 'scissors' && computerSelection === 'rock') {
-        computerScore ++;
-        return 'loser';
-    } else if (playerSelection === computerSelection) {
-        return 'draw';
+const buttons = document.querySelectorAll('button');
+const playerScoreElement = document.querySelector('#player-score');
+const computerScoreElement = document.querySelector('#computer-score');
+
+//this function returns rock/paper/scissors for computer
+getComputerChoice = () => {
+    let random = Math.floor(Math.random() * 3);
+    if (random === 0) {
+        computerSelection = 'Rock';
+    } else if (random === 1) {
+        computerSelection = 'Paper';
+    } else if (random === 2) {
+        computerSelection = 'Scissors';
     }
+    return computerSelection;
 }
 
-function game() {
-    for (i = 0; i < 5; i++) {
-        playerSelection = prompt('rock paper or scissors');
-        while (playerSelection != 'rock' && playerSelection != 'paper' && playerSelection != 'scissors') {
-            playerSelection = prompt('try again pls');
+//this function compares players choice with the computers', and increases the score
+const playRound = (playerSelection) => {
+    getComputerChoice();
+    if (playerSelection === 'Rock' && computerSelection === 'Paper') {
+        computerScore++;
+    } else if (playerSelection === 'Rock' && computerSelection === 'Scissors') {
+        playerScore++;
+    } else if (playerSelection === 'Paper' && computerSelection === 'Rock') {
+        playerScore++;
+    } else if (playerSelection === 'Paper' && computerSelection === 'Scissors') {
+        computerScore++;
+    } else if (playerSelection === 'Scissors' && computerSelection === 'Paper') {
+        playerScore++;;
+    } else if (playerSelection === 'Scissors' && computerSelection === 'Rock') {
+        computerScore++;
+    }//  else if (playerSelection === computerSelection) {
+        // return 'draw';
+    // }
+    playerScoreElement.innerText = `Player Score: ${playerScore}`;
+    computerScoreElement.innerText = `Computer Score: ${computerScore}`;
+    // console.log(getComputerChoice);
+}
+
+//on click event returns rock/paper/scissors to playerSelection, and plays a round
+buttons.forEach((button) => {
+    button.addEventListener('click', (e) => {
+        if (playerScore < 5 && computerScore < 5) {
+            playerSelection = e.target.innerText;
+            playRound(playerSelection);
+            if (playerScore === 5) {
+                createConclusionScreen('winner');
+            } else if (computerScore === 5) {
+                createConclusionScreen('loser');
+            }
         }
-        computerSelection = getComputerChoice();
-        console.log(playRound(playerSelection, computerSelection));
-        console.log(`player score: ${playerScore}\ncomputer score: ${computerScore}`);
+    })
+})
+
+//creates another pannel, declaring you as a winner/loser
+const createConclusionScreen = (outcome) => {
+    const body = document.querySelector('body');
+    const conclusionScreen = document.createElement('div');
+    conclusionScreen.classList.add('conclusion-screen');
+    conclusionScreen.innerText = outcome;
+    if (outcome === 'winner') {
+        conclusionScreen.style.backgroundColor = 'lightskyblue';
     }
+    body.append(conclusionScreen);
 }
-
-game();
-
-if (playerScore > computerScore) {
-    console.log('You are the winner! gz gz! :))');
-} else if (computerScore > playerScore) {
-    console.log('You are a loser my friend :(');
-} else {
-    console.log(`It's a tie :|`);
-}
-
-/*
-computer gets a random choice
-ask user for 1 of those choices
-it has to be a valid one
-calculate and display the result
-*/
